7e0fbec073bcbb2aa8a10b74c90815e3b87cd3cb
formlc <mcmahonhc@gmail.com>
2017-10-21 13:09:56 -0500

    nope

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
 
-	
+	var APIkey = "00528d89e0954e6f9b6e5e992d064f42"; 
+
+	$("button").on("click", function() {
+
+	var userInput = $("#input").val();
+
+	var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&page=2&sort=oldest&api-key=" + APIkey;
+
+	$.ajax({
+	  url: url,
+	  method: 'GET',
+	}).done(function(result) {
+	  console.log(result);
+	}).fail(function(err) {
+	  throw err;
+	});
+
+
+
+});
 
 });
